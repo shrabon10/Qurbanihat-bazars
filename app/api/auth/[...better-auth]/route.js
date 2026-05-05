e4eee03284a494a7f4fe1b999769aeb40fae4cbf
shrabon10 <omarfaruksrabon@gmail.com>
2026-05-05 23:41:54 +0600

    MongoDb Connected Succesfully and workable Data fetch

--- app/api/auth/[...better-auth]/route.js
+++ app/api/auth/[...better-auth]/route.js
@@ -1,9 +1,3 @@
-// import { auth } from "@/lib/auth";
-// import { toNextJsHandler } from "better-auth/next-js";
-
-// export const { GET, POST } = toNextJsHandler(auth);
-
-
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
